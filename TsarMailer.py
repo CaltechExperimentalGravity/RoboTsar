5cac2e865d941e61b6ccb5139577be1e09333a5d
Andrew Wade <fincle@users.noreply.github.com>
2017-01-20 12:52:49 -0800

    Updated, first working version. Now sends message but still beta with password in plaintext.  Next step to enact google credentials standard method using their API.

--- TsarMailer.py
+++ TsarMailer.py
@@ -17,36 +17,11 @@ USR = "JournalClubRoboTsar@gmail.com"
 PSSSWD = "" # This is temp measure until seperate key file can be enacted drawn from local dir
 
 
-
-
-
 # Some intial test values for email addresses
 me = "JournalClubRoboTsar@gmail.com"
 you = "wadean@gmail.com"
 
-
-
 def main():
-
-    # # Now generate the body content of the email
-    # text = "Hi anon!\n This is a first message with some line breaks.\n \n And here is the rest of the message.\n You are welcome."
-    # html = """\
-    # <html>
-    #   <head></head>
-    #   <body>
-    #     <p>Hi anon!<br>
-    #        This is a first message with some line breaks.<br>
-    #        This version is the HTML formatted message
-    #         Here is a cool link: <a href="https://www.python.org">link</a>.
-    #     </p>
-    #   </body>
-    # </html>
-    # """
-
-
-
-    # s = smtplib.SMTP('localhost') # Send the message via local SMTP server.
-
     # Connect with custom server
     s = smtplib.SMTP()
     s.connect(HOST,PORT)
@@ -57,8 +32,9 @@ def main():
 
 
     # sendmail function takes 3 arguments: sender's address, recipient's address
-    # and message to send - here it is sent as one string.
-    s.sendmail(me, you, msg.as_string())
+    # and message to send - here it is sent as one string using the create_message sub-routien
+    s.sendmail(me, you, create_message(me,you,"Test subject","All the text"))
+    # s.sendmail(me, you, msg.as_string())
     s.quit()
 
 
@@ -93,8 +69,8 @@ def create_message(sender,to,subject,message_text):
     # the HTML message, is best and preferred.
     msg.attach(msg_plaintext)
     msg.attach(msg_html)
-    return {'raw': base64.urlsafe_b64encode(msg.as_string())}
-
+    # return {'raw': base64.urlsafe_b64encode(msg.as_string())} # This was from the google API example doesnt work yet
+    return msg.as_string()
 
 
 if __name__ == '__main__':
